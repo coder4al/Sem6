fa2dc2841aef8772f305d511798b96430167daa0
coder4al <coder4al@gmail.com>
2024-04-23 01:11:41 +0530

    Invalid Input Problem Solved

--- WT/5-Arithmetic/script.js
+++ WT/5-Arithmetic/script.js
@@ -2,22 +2,26 @@ function calculate(operation) {
   const num1 = parseFloat(prompt("Enter First Number: "));
   const num2 = parseFloat(prompt("Enter Second Number: "));
 
-  switch (operation) {
-    case "A":
-      alert("Sum is " + (num1 + num2));
-      break;
-    case "S":
-      alert("Difference is " + (num1 - num2));
-      break;
-    case "M":
-      alert("Product is " + num1 * num2);
-      break;
-    case "D":
-      if (num2 !== 0) {
-        alert("Quotient is " + num1 / num2);
-      } else {
-        alert("Division by zero is not allowed");
-      }
-      break;
+  if (!num1 || !num2) {
+    alert("Invalid Input");
+  } else {
+    switch (operation) {
+      case "A":
+        alert("Sum is " + (num1 + num2));
+        break;
+      case "S":
+        alert("Difference is " + (num1 - num2));
+        break;
+      case "M":
+        alert("Product is " + num1 * num2);
+        break;
+      case "D":
+        if (num2 !== 0) {
+          alert("Quotient is " + num1 / num2);
+        } else {
+          alert("Division by zero is not allowed");
+        }
+        break;
+    }
   }
 }
